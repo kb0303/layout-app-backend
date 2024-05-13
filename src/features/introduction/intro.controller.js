375c070cd3f8a815d5d94644673b209a0b6215e7
pixysq <bishtkrishnam400@gmail.com>
2024-05-13 14:31:47 +0530

    changes

--- src/features/introduction/intro.controller.js
+++ src/features/introduction/intro.controller.js
@@ -8,8 +8,13 @@ export default class IntroController {
 
 	async getAll(req, res) {
 		try {
+			const startTime = new Date(); // Start time
 			// Fetch all data from the repository
 			const data = await this.introRepository.getAll();
+			const endTime = new Date(); // End time
+			const executionTime = endTime - startTime; // Time taken in milliseconds
+			console.log('Total time taken to process getAll request:', executionTime, 'milliseconds');
+
 			res.json(data);
 		} catch (error) {
 			console.error('Error fetching all data:', error);
@@ -19,6 +24,7 @@ export default class IntroController {
 
 	async add(req, res) {
 		try {
+			const startTime = new Date(); // Start time
 			const { name, age, place } = req.body;
 			const imageUrl = req.file.filename
 
@@ -27,6 +33,10 @@ export default class IntroController {
 
 			// adding content in the database
 			await this.introRepository.add(newContent);
+			const endTime = new Date(); // End time
+			const executionTime = endTime - startTime; // Time taken in milliseconds
+			console.log('Total time taken to process add request:', executionTime, 'milliseconds');
+
 			res.status(201).send(newContent);
 
 		} catch (error) {
@@ -36,6 +46,7 @@ export default class IntroController {
 
 	async update(req, res) {
 		try {
+			const startTime = new Date(); // Start time
 			const { name, age, place } = req.body;
 			const id = req.params.id;
 
@@ -43,11 +54,14 @@ export default class IntroController {
 
 			// Checking if file exists
 			if (req.file) {
-				imageUrl = req.file.filename; 
+				imageUrl = req.file.filename;
 			}
 
 			// Calling repository update method
 			const updatedContent = await this.introRepository.update(name, age, place, imageUrl, id);
+			const endTime = new Date(); // End time
+			const executionTime = endTime - startTime; // Time taken in milliseconds
+			console.log('Total time taken to process update request:', executionTime, 'milliseconds');
 
 			if (!updatedContent) {
 				return res.status(404).send('Content not found');
@@ -60,11 +74,15 @@ export default class IntroController {
 		}
 	}
 
-
 	async reset(req, res) {
 		try {
+			const startTime = new Date(); // Start time
 			// Call the repository's reset method
 			const deletedCount = await this.introRepository.reset();
+			const endTime = new Date(); // End time
+			const executionTime = endTime - startTime; // Time taken in milliseconds
+			console.log('Total time taken to process reset request:', executionTime, 'milliseconds');
+
 			res.status(200).send(`${deletedCount} documents deleted successfully.`);
 		} catch (error) {
 			console.error('Error resetting database:', error);
